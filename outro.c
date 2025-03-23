2ae4b8fbd8d033fda10f0b6ec1c014fd8b4102ae
Wallows <valeriasan272@gmail.com>
2025-03-23 16:12:26 -0300

    quinto commit

---
+++ outro.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	printf("Outro programa");
+
+	return 0;
+}
---
+++ teste.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+//main -> principal
+int main(){
+	printf("Geek University");
+
+	return 0;
+}
